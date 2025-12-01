22c7c9440d34a2550082ac7f8cab1eefa7f06025
nassaigael <hei.nassai@gmail.com>
2025-12-01 13:30:36 +0300

    first commit

---
+++ src/main/java/com/fizanakara/config/ConnectDatabase.java
@@ -0,0 +1,9 @@
+package com.fizanakara.config;
+
+public class ConnectDatabase {
+    private static final String url = "jdbc:postgresql://localhost:5432/";
+    private static final String user = "postgres";
+    private static final String password = "PassNassaIWord";
+
+
+}
